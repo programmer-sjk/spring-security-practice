32c919052bccecc2952c5fcb478409f51052a887
sjk5766 <wjdrnr6662@gmail.com>
2023-02-21 01:27:02 +0900

    feat: Security로 접근 가능/ 인증 필요한 접근 설정

---
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/users")
+                            .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/health")
+                            .permitAll()
+                        .anyRequest()
+                            .authenticated()
+                )
+                .csrf().disable()
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
