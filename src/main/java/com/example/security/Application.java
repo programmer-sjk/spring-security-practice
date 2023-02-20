dbf1325d91d2c17d26365c27c4a6ed0c28ae0e21
sjk5766 <wjdrnr6662@gmail.com>
2023-02-20 15:51:43 +0900

    refactor: Application.java로 이름 수정

--- src/main/java/com/example/security/Application.java
+++ src/main/java/com/example/security/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
