61dffccf543a5e950e2f56d1ce2d152f68bf4654
sjk5766 <wjdrnr6662@gmail.com>
2023-02-24 21:00:09 +0900

    feat: security config에 filter 적용

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.example.security.config;
 
 import com.example.security.exception.AccessDenyHandler;
 import com.example.security.exception.AuthEntryPoint;
+//import com.example.security.filter.AfterFilter;
+import com.example.security.filter.AfterFilter;
+import com.example.security.filter.BeforeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.*;
 
@@ -19,10 +23,14 @@ import static org.springframework.security.config.Customizer.*;
 public class SecurityConfig {
     private final AuthEntryPoint authEntryPoint;
     private final AccessDenyHandler accessDenyHandler;
+    private final BeforeFilter beforeFilter;
+    private final AfterFilter afterFilter;
 
-    public SecurityConfig(AuthEntryPoint authEntryPoint, AccessDenyHandler accessDenyHandler) {
+    public SecurityConfig(AuthEntryPoint authEntryPoint, AccessDenyHandler accessDenyHandler, BeforeFilter beforeFilter, AfterFilter afterFilter) {
         this.authEntryPoint = authEntryPoint;
         this.accessDenyHandler = accessDenyHandler;
+        this.beforeFilter = beforeFilter;
+        this.afterFilter = afterFilter;
     }
 
     @Bean
@@ -39,6 +47,8 @@ public class SecurityConfig {
                 )
                 .csrf().disable()
                 .httpBasic(withDefaults())
+                .addFilterBefore(beforeFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(afterFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling()
                     .authenticationEntryPoint(authEntryPoint)
                     .accessDeniedHandler(accessDenyHandler).and()
