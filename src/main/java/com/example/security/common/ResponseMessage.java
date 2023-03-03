22164ef5911a4a89dcd9d0ec4e77f9238887b43e
sjk5766 <wjdrnr6662@gmail.com>
2023-03-03 16:34:15 +0900

    feat: ResponseMessage data와 statusCode 추가

--- src/main/java/com/example/security/common/ResponseMessage.java
+++ src/main/java/com/example/security/common/ResponseMessage.java
@@ -1,23 +1,53 @@
 package com.example.security.common;
 
-public class ResponseMessage {
-    private String message;
+import org.springframework.http.HttpStatus;
 
-    public static ResponseMessage unauthorized() {
-        return new ResponseMessage("인증이 필요합니다.");
+public class ResponseMessage<T> {
+    private int statusCode = HttpStatus.OK.value();
+    private String message = "";
+    protected T data = (T) "";
+
+    public static ResponseMessage<String> ok() {
+        return new ResponseMessage<>("");
     }
 
-    public static ResponseMessage forbidden() {
-        return new ResponseMessage("권한이 필요합니다.");
+    public static <T> ResponseMessage<T> ok(T data) {
+        return new ResponseMessage<>(data);
     }
 
-    private ResponseMessage() {}
+    public static ResponseMessage<String> badRequest(String message) {
+        return new ResponseMessage<>(HttpStatus.BAD_REQUEST.value(), message, "");
+    }
 
-    private ResponseMessage(String message) {
+    public static ResponseMessage<String> unauthorized(String message) {
+        return new ResponseMessage<>(HttpStatus.UNAUTHORIZED.value(), message, "");
+    }
+
+    public static ResponseMessage<String> forbidden(String message) {
+        return new ResponseMessage<>(HttpStatus.FORBIDDEN.value(), message, "");
+    }
+
+    protected ResponseMessage(T data) {
+        this.data = data;
+    }
+
+    private ResponseMessage(int statusCode, String message, T data) {
+        this.statusCode = statusCode;
         this.message = message;
+        if (data != null) {
+            this.data = data;
+        }
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public T getData() {
+        return data;
     }
 }
